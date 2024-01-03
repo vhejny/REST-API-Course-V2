c4105293fe34105ac7f7abc057101a033ffa1e13
vhejny <155542902+vhejny@users.noreply.github.com>
2024-01-03 13:39:13 +0100

    connected to mongodb

--- db/clouddb.js
+++ db/clouddb.js
@@ -4,10 +4,12 @@
 
  // mongodb+srv://admin:<password>@cluster0-46e5h.mongodb.net/test?retryWrites=true&w=majority
 
-const DB_USER = "admin"
+ //mongodb+srv://vithejny:<password>@cluster0.yvehn72.mongodb.net/?retryWrites=true&w=majority
+
+const DB_USER = "vithejny"
 const DB_PASSWORD = "admin123"
 const DB_NAME = "acmetravel"
-const CLUSTER_HOST = "cluster0-46e5h.mongodb.net"
+const CLUSTER_HOST = "cluster0.yvehn72.mongodb.net"
 
 // Setup the DB URI
 exports.DB_URI= "mongodb+srv://"+DB_USER+":"+DB_PASSWORD+"@"+CLUSTER_HOST+"/"+DB_NAME+"?retryWrites=true&w=majority"
